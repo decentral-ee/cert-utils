6afe8d77e6481654c9b05efc180fe6f35289cb1b
Miao ZhiCheng <hellwolf.misty@gmail.com>
2020-01-08 14:50:49 +0200

    Create test-ecdh1-derive.sh

---
+++ test-ecdh1-derive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openssl pkeyutl -derive -inkey data/new-secp384r1-private-key.pem -peerkey data/38601300071-pin1-public-key.pem | xxd -p | tr '\n' '|' | sed 's/|//g'
+echo
+
+pkcs11-tool --derive --token-label PIN1 --login --id 01 -m ECDH1-DERIVE --input-file data/new-secp384r1-public-key.der --output-file >(xxd -p | tr '\n' '|' | sed 's/|//g')
